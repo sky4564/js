311e77c0060b2f02c6ee6e1b71314e7e6bb9cf57
김병민 <kbm5553@gnifrix.com>
2024-02-28 11:35:45 +0900

    slice 정리

---
+++ slice.js
@@ -0,0 +1,19 @@
+/**
+ * @용도 : 배열의 값 반환
+ * @사용 : 문자열도 배열로 취급되서 문자열에도 사용가능
+ * @반환값 : 문자열에서는 문자열 , 배열에서는 slice 해당하는 배열을 포함한 배열
+ * @특이점 : splice 와 비교하여 얕은복사로 원본배열의 변경없음
+ */
+
+let arr = [1, 2, 3]
+let obj = {
+  test: "is objcet test",
+  boolean: true,
+}
+let str = 'isTestString'
+let stringArr = [1,2,3,4,5]
+
+
+let test = str.slice(0,2)
+
+console.log(test)
