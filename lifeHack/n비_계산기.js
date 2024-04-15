73a388238d1137453fc9f8ec023c3e5e1e682a29
byoung min kim <kbm5553@gnifrix.com>
2024-04-15 14:50:53 +0900

    N비 계산기 업뎃

--- lifeHack/n비_계산기.js
+++ lifeHack/n비_계산기.js
@@ -1,19 +1,30 @@
 // n비 계산에 참여하는 인원   = n 명으로
 // 술은 안먹는 깎두기   = k
 
-let bill = [71000, 35500, 8000]
-let alcPrice = 25000
-let n = 4 // 뽀 쏭 민 정
-let k = 1 // 뽀
-//==============================================//
+cal_nonAlc_table = (bill, alcPrice, n, k) => {
+  let sum = bill.reduce((acc, cur) => acc + cur);
+  let pay = (sum - alcPrice) / n;
+  let alcFee = Math.floor(alcPrice / (n - k));
 
-let sum = bill.reduce((acc,cur)=> acc + cur)
+  let result = {
+    총합: sum,
+    술쟁이들: pay + alcFee,
+    노술쟁이: pay,
+  };
+  return result;
+};
+
+cal_alc_table = (bill, n) => {
+  let sum = bill.reduce((acc, cur) => acc + cur);
+  let pay = sum / n;
+  let result = {
+    총합: sum,
+    술쟁이들: pay,
+  };
+  return result;
+};
 
 
-let pay = (sum-alcPrice) / n
-let alcFee = Math.floor(alcPrice / (n - k))
 
-console.log('인당 내야 할 금액입니다.' ,  pay ,'원')
-console.log('술쟁이들이 추가로 내야 할 금액입니다.' ,  alcFee ,'원')
-console.log('술쟁이들' ,  pay +alcFee ,'원')
-console.log('보라   ' ,  pay ,'원')
+console.log(cal_nonAlc_table([71000, 35500, 8000], 25000, 4, 1));
+console.log(cal_alc_table([71000, 35500, 8000], 4));
