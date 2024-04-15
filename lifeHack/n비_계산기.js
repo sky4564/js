b6ad76a874fdb6ed48ef1d9445946d2ba72755a7
byoung min kim <kbm5553@gnifrix.com>
2024-04-15 15:07:13 +0900

    프리티어 설정

--- lifeHack/n비_계산기.js
+++ lifeHack/n비_계산기.js
@@ -2,29 +2,27 @@
 // 술은 안먹는 깎두기   = k
 
 cal_nonAlc_table = (bill, alcPrice, n, k) => {
-  let sum = bill.reduce((acc, cur) => acc + cur);
-  let pay = (sum - alcPrice) / n;
-  let alcFee = Math.floor(alcPrice / (n - k));
-
-  let result = {
-    총합: sum,
-    술쟁이들: pay + alcFee,
-    노술쟁이: pay,
-  };
-  return result;
+	let sum = bill.reduce((acc, cur) => acc + cur);
+	let pay = (sum - alcPrice) / n;
+	let alcFee = Math.floor(alcPrice / (n - k));
+	let result = {
+		총합: sum,
+		술쟁이들: pay + alcFee,
+		노술쟁이: pay,
+	};
+	return result;
 };
 
 cal_alc_table = (bill, n) => {
-  let sum = bill.reduce((acc, cur) => acc + cur);
-  let pay = sum / n;
-  let result = {
-    총합: sum,
-    술쟁이들: pay,
-  };
-  return result;
+	let sum = bill.reduce((acc, cur) => acc + cur);
+	let pay = sum / n;
+	let result = {
+		//맨마지막 줄 넣는지 여부
+		총합: sum,
+		술쟁이들: pay,
+	};
+	return result;
 };
-
-
 
 console.log(cal_nonAlc_table([71000, 35500, 8000], 25000, 4, 1));
 console.log(cal_alc_table([71000, 35500, 8000], 4));
